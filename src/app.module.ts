22b50e5309a1ed951a85d5ed3009285147edf50e
Sergio Mercado <mercadosergio754@gmail.com>
2026-03-24 19:18:09 -0500

    fix prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { ClientsModule } from './app/clients/clients.module';
 import { OrdersModule } from './app/orders/orders.module';
 import { PrismaModule } from './config/prisma/prisma.module';
-import { PrismaService } from './config/prisma/prisma.service';
 
 @Module({
   imports: [ClientsModule, OrdersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
