342c0ada1a0c81b12eec58f043ff45e0dd04a55e
Sergio Mercado <mercadosergio754@gmail.com>
2026-03-24 19:07:42 -0500

    fix validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,14 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './app/clients/clients.module';
 import { OrdersModule } from './app/orders/orders.module';
 import { PrismaModule } from './config/prisma/prisma.module';
 import { PrismaService } from './config/prisma/prisma.service';
-import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ClientsModule, OrdersModule, PrismaModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    PrismaService,
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
-  ],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
